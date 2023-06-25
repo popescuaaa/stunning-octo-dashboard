173e439ca23ada6f114863fa74f91d8b862618d3
Andrei Popescu <andreigabrielpopescu556@gmail.com>
2023-06-25 16:28:08 +0300

    sidebar

--- client/src/partials/Sidebar.jsx
+++ client/src/partials/Sidebar.jsx
@@ -54,9 +54,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 		<div>
 			{/* Sidebar backdrop (mobile only) */}
 			<div
-				className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
-					sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-				}`}
+				className={`fixed inset-0 bg-gray-200 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+					}`}
 				aria-hidden="true"
 			></div>
 
@@ -64,9 +63,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 			<div
 				id="sidebar"
 				ref={sidebar}
-				className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
-					sidebarOpen ? 'translate-x-0' : '-translate-x-64'
-				}`}
+				className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-64'
+					}`}
 			>
 				{/* Sidebar header */}
 				<div className="flex justify-between mb-10 pr-3 sm:px-2">
@@ -122,11 +120,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													(pathname === '/' ||
-														pathname.includes('dashboard')) &&
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${(pathname === '/' ||
+													pathname.includes('dashboard')) &&
 													'hover:text-slate-200'
-												}`}
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -141,27 +138,24 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 															viewBox="0 0 24 24"
 														>
 															<path
-																className={`fill-current text-slate-400 ${
-																	(pathname === '/' ||
-																		pathname.includes('dashboard')) &&
+																className={`fill-current text-slate-400 ${(pathname === '/' ||
+																	pathname.includes('dashboard')) &&
 																	'!text-indigo-500'
-																}`}
+																	}`}
 																d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z"
 															/>
 															<path
-																className={`fill-current text-slate-600 ${
-																	(pathname === '/' ||
-																		pathname.includes('dashboard')) &&
+																className={`fill-current text-slate-600 ${(pathname === '/' ||
+																	pathname.includes('dashboard')) &&
 																	'text-indigo-600'
-																}`}
+																	}`}
 																d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z"
 															/>
 															<path
-																className={`fill-current text-slate-400 ${
-																	(pathname === '/' ||
-																		pathname.includes('dashboard')) &&
+																className={`fill-current text-slate-400 ${(pathname === '/' ||
+																	pathname.includes('dashboard')) &&
 																	'text-indigo-200'
-																}`}
+																	}`}
 																d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"
 															/>
 														</svg>
@@ -184,10 +178,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													pathname.includes('ecommerce') &&
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${pathname.includes('ecommerce') &&
 													'hover:text-slate-200'
-												}`}
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -202,24 +195,21 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 															viewBox="0 0 24 24"
 														>
 															<path
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('ecommerce') &&
+																className={`fill-current text-slate-400 ${pathname.includes('ecommerce') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																d="M13 15l11-7L11.504.136a1 1 0 00-1.019.007L0 7l13 8z"
 															/>
 															<path
-																className={`fill-current text-slate-700 ${
-																	pathname.includes('ecommerce') &&
+																className={`fill-current text-slate-700 ${pathname.includes('ecommerce') &&
 																	'!text-indigo-600'
-																}`}
+																	}`}
 																d="M13 15L0 7v9c0 .355.189.685.496.864L13 24v-9z"
 															/>
 															<path
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('ecommerce') &&
+																className={`fill-current text-slate-600 ${pathname.includes('ecommerce') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																d="M13 15.047V24l10.573-7.181A.999.999 0 0024 16V8l-11 7.047z"
 															/>
 														</svg>
@@ -230,9 +220,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 													{/* Icon */}
 													<div className="flex shrink-0 ml-2">
 														<svg
-															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-																open && 'rotate-180'
-															}`}
+															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'
+																}`}
 															viewBox="0 0 12 12"
 														>
 															<path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
@@ -288,9 +277,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													pathname.includes('tasks') && 'hover:text-slate-200'
-												}`}
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${pathname.includes('tasks') && 'hover:text-slate-200'
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -305,24 +293,21 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 															viewBox="0 0 24 24"
 														>
 															<path
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('tasks') &&
+																className={`fill-current text-slate-600 ${pathname.includes('tasks') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																d="M8 1v2H3v19h18V3h-5V1h7v23H1V1z"
 															/>
 															<path
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('tasks') &&
+																className={`fill-current text-slate-600 ${pathname.includes('tasks') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																d="M1 1h22v23H1z"
 															/>
 															<path
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('tasks') &&
+																className={`fill-current text-slate-400 ${pathname.includes('tasks') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																d="M15 10.586L16.414 12 11 17.414 7.586 14 9 12.586l2 2zM5 0h14v4H5z"
 															/>
 														</svg>
@@ -338,29 +323,25 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 							</SidebarLinkGroup>
 							{/* Campaigns */}
 							<li
-								className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
-									pathname.includes('campaigns') && 'bg-slate-900'
-								}`}
+								className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('campaigns') && 'bg-slate-900'
+									}`}
 							>
 								<NavLink
 									end
 									to="/"
-									className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-										pathname.includes('campaigns') && 'hover:text-slate-200'
-									}`}
+									className={`block text-slate-200 hover:text-white truncate transition duration-150 ${pathname.includes('campaigns') && 'hover:text-slate-200'
+										}`}
 								>
 									<div className="flex items-center">
 										<svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
 											<path
-												className={`fill-current text-slate-600 ${
-													pathname.includes('campaigns') && 'text-indigo-500'
-												}`}
+												className={`fill-current text-slate-600 ${pathname.includes('campaigns') && 'text-indigo-500'
+													}`}
 												d="M20 7a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 0120 7zM4 23a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 014 23z"
 											/>
 											<path
-												className={`fill-current text-slate-400 ${
-													pathname.includes('campaigns') && 'text-indigo-300'
-												}`}
+												className={`fill-current text-slate-400 ${pathname.includes('campaigns') && 'text-indigo-300'
+													}`}
 												d="M17 23a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 010-2 4 4 0 004-4 1 1 0 012 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1zM7 13a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 110-2 4 4 0 004-4 1 1 0 112 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1z"
 											/>
 										</svg>
@@ -377,10 +358,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													pathname.includes('settings') &&
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${pathname.includes('settings') &&
 													'hover:text-slate-200'
-												}`}
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -395,31 +375,27 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 															viewBox="0 0 24 24"
 														>
 															<path
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('settings') &&
+																className={`fill-current text-slate-600 ${pathname.includes('settings') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																d="M19.714 14.7l-7.007 7.007-1.414-1.414 7.007-7.007c-.195-.4-.298-.84-.3-1.286a3 3 0 113 3 2.969 2.969 0 01-1.286-.3z"
 															/>
 															<path
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('settings') &&
+																className={`fill-current text-slate-400 ${pathname.includes('settings') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.885.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"
 															/>
 															<path
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('settings') &&
+																className={`fill-current text-slate-600 ${pathname.includes('settings') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																d="M5.7 10.714c.195.4.298.84.3 1.286a3 3 0 11-3-3c.446.002.885.105 1.286.3l7.007-7.007 1.414 1.414L5.7 10.714z"
 															/>
 															<path
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('settings') &&
+																className={`fill-current text-slate-400 ${pathname.includes('settings') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"
 															/>
 														</svg>
@@ -430,9 +406,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 													{/* Icon */}
 													<div className="flex shrink-0 ml-2">
 														<svg
-															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-																open && 'rotate-180'
-															}`}
+															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'
+																}`}
 															viewBox="0 0 12 12"
 														>
 															<path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
@@ -488,9 +463,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													pathname.includes('utility') && 'hover:text-slate-200'
-												}`}
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${pathname.includes('utility') && 'hover:text-slate-200'
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -505,37 +479,33 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 															viewBox="0 0 24 24"
 														>
 															<circle
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('utility') &&
+																className={`fill-current text-slate-400 ${pathname.includes('utility') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																cx="18.5"
 																cy="5.5"
 																r="4.5"
 															/>
 															<circle
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('utility') &&
+																className={`fill-current text-slate-600 ${pathname.includes('utility') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																cx="5.5"
 																cy="5.5"
 																r="4.5"
 															/>
 															<circle
-																className={`fill-current text-slate-600 ${
-																	pathname.includes('utility') &&
+																className={`fill-current text-slate-600 ${pathname.includes('utility') &&
 																	'text-indigo-500'
-																}`}
+																	}`}
 																cx="18.5"
 																cy="18.5"
 																r="4.5"
 															/>
 															<circle
-																className={`fill-current text-slate-400 ${
-																	pathname.includes('utility') &&
+																className={`fill-current text-slate-400 ${pathname.includes('utility') &&
 																	'text-indigo-300'
-																}`}
+																	}`}
 																cx="5.5"
 																cy="18.5"
 																r="4.5"
@@ -548,9 +518,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 													{/* Icon */}
 													<div className="flex shrink-0 ml-2">
 														<svg
-															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-																open && 'rotate-180'
-															}`}
+															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'
+																}`}
 															viewBox="0 0 12 12"
 														>
 															<path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
@@ -622,9 +591,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 										<React.Fragment>
 											<a
 												href="#0"
-												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-													open && 'hover:text-slate-200'
-												}`}
+												className={`block text-slate-200 hover:text-white truncate transition duration-150 ${open && 'hover:text-slate-200'
+													}`}
 												onClick={(e) => {
 													e.preventDefault();
 													sidebarExpanded
@@ -654,9 +622,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 													{/* Icon */}
 													<div className="flex shrink-0 ml-2">
 														<svg
-															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-																open && 'rotate-180'
-															}`}
+															className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'
+																}`}
 															viewBox="0 0 12 12"
 														>
 															<path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
